01a719919b5320c2a940e912738ce82dfcc4a01b
bnw_ohnami <ohnami@bijutsu.co.jp>
2015-01-27 19:26:40 +0900

    SmartyBaseHelper読み込み場所の修正
    元の読み込み場所はSmartyBaseHelperが使用される後に記述されている為
    Class 'SmartyBaseHelper' not found
    が発生する

--- View/SmartyView.php
+++ View/SmartyView.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * CakePHP Smarty view class
  *
@@ -13,11 +14,10 @@
  * @since        CakePHP v 2.0
  * @license      MIT License
  */
-
 /**
  * Include Smarty.
  */
-App::import('Vendor', 'Smarty', array('file' => 'smarty'.DS.'Smarty.class.php'));
+App::import('Vendor', 'Smarty', array('file' => 'smarty' . DS . 'Smarty.class.php'));
 
 /**
  * CakePHP Smarty view class
@@ -28,12 +28,11 @@ App::import('Vendor', 'Smarty', array('file' => 'smarty'.DS.'Smarty.class.php'))
  * @subpackage   view
  * @since        CakePHP v 2.0
  */
-class SmartyView extends View
-{
+class SmartyView extends View {
+
     public $loaded = array();
     public $ext = '.tpl';
     public $pluginName = 'SmartyView';
-
     public $smartyVersion;
     public $smartyMajorVersion;
 
@@ -42,69 +41,64 @@ class SmartyView extends View
      *
      * @param  $controller instance of calling controller
      */
-	function __construct (&$controller)
-	{
-		parent::__construct($controller);
-
-		$this->Smarty = new Smarty();
-		$this->_smartyVersion();
-
-		$this->ext= '.tpl';
-		$this->Smarty->compile_dir = TMP.'smarty'.DS.'compile'.DS;
-		$this->Smarty->cache_dir = TMP.'smarty'.DS.'cache'.DS;
-		$this->Smarty->error_reporting = 'E_ALL & ~E_NOTICE';
-		$this->Smarty->debugging = true;
-		$this->Smarty->caching = 0;
-        switch ($this->smartyMajorVersion) {
-            case 2:
-                $this->Smarty->clear_compiled_tpl();
-                $this->Smarty->plugins_dir[] = APP . 'View' . DS.'smarty_plugins'.DS;
-                break;
-            case 3:
-                $this->Smarty->clearCompiledTemplate();
-                $this->Smarty->setPluginsDir(array(APP . 'View' . DS.'smarty_plugins'.DS));
-                break;
-        }
+    function __construct(&$controller) {
 
         // Loading base class of Smarty Helpers
         App::uses('SmartyBaseHelper', $this->pluginName.'.'.'View/Helper');
-	}
+        
+        parent::__construct($controller);
 
-/**
- * Overrides the View::_render()
- * Sets variables used in CakePHP to Smarty variables
- *
- * @param string $___viewFn
- * @param string $___data_for_view
- * @param string $___play_safe
- * @param string $loadHelpers
- * @return rendered views
- */
-    function _render($___viewFn, $___data_for_view = array())
-	{
+        $this->Smarty = new Smarty();
+        $this->_smartyVersion();
+
+        $this->ext = '.tpl';
+        $this->Smarty->compile_dir = TMP . 'smarty' . DS . 'compile' . DS;
+        $this->Smarty->cache_dir = TMP . 'smarty' . DS . 'cache' . DS;
+        $this->Smarty->error_reporting = 'E_ALL & ~E_NOTICE';
+        $this->Smarty->debugging = true;
+        $this->Smarty->caching = 0;
+        switch ($this->smartyMajorVersion) {
+            case 2:
+                $this->Smarty->clear_compiled_tpl();
+                $this->Smarty->plugins_dir[] = APP . 'View' . DS . 'smarty_plugins' . DS;
+                break;
+            case 3:
+                $this->Smarty->clearCompiledTemplate();
+                $this->Smarty->setPluginsDir(array(APP . 'View' . DS . 'smarty_plugins' . DS));
+                break;
+        }
+    }
+
+    /**
+     * Overrides the View::_render()
+     * Sets variables used in CakePHP to Smarty variables
+     *
+     * @param string $___viewFn
+     * @param string $___data_for_view
+     * @param string $___play_safe
+     * @param string $loadHelpers
+     * @return rendered views
+     */
+    function _render($___viewFn, $___data_for_view = array()) {
         // use cake's render for .ctp files. 
-        if (preg_match('/\.ctp$/', $___viewFn)) { 
+        if (preg_match('/\.ctp$/', $___viewFn)) {
             return parent::_render($___viewFn, $___data_for_view);
-        } 
-        if ($this->helpers != false)
-		{
+        }
+        if ($this->helpers != false) {
             $this->loadHelpers();
             $loadedHelpers = $this->helpers;
 
-			foreach(array_keys($loadedHelpers) as $helper)
-			{
-				$replace = strtolower(substr($helper, 0, 1));
-				$camelBackedHelper = preg_replace('/\\w/', $replace, $helper, 1);
+            foreach (array_keys($loadedHelpers) as $helper) {
+                $replace = strtolower(substr($helper, 0, 1));
+                $camelBackedHelper = preg_replace('/\\w/', $replace, $helper, 1);
 
-				${$camelBackedHelper} =& $loadedHelpers[$helper];
-				if(isset(${$camelBackedHelper}->helpers) && is_array(${$camelBackedHelper}->helpers))
-				{
-					foreach(${$camelBackedHelper}->helpers as $subHelper)
-					{
-						${$camelBackedHelper}->{$subHelper} =& $loadedHelpers[$subHelper];
-					}
-				}
-				$this->loaded[$camelBackedHelper] = (${$camelBackedHelper});
+                ${$camelBackedHelper} = & $loadedHelpers[$helper];
+                if (isset(${$camelBackedHelper}->helpers) && is_array(${$camelBackedHelper}->helpers)) {
+                    foreach (${$camelBackedHelper}->helpers as $subHelper) {
+                        ${$camelBackedHelper}->{$subHelper} = & $loadedHelpers[$subHelper];
+                    }
+                }
+                $this->loaded[$camelBackedHelper] = (${$camelBackedHelper});
                 switch ($this->smartyMajorVersion) {
                     case 2:
                         $this->Smarty->assign_by_ref($camelBackedHelper, ${$camelBackedHelper});
@@ -113,21 +107,19 @@ class SmartyView extends View
                         $this->Smarty->assignByRef($camelBackedHelper, ${$camelBackedHelper});
                         break;
                 }
-			}
-		}
+            }
+        }
 
-		$this->registerFunctions();
+        $this->registerFunctions();
 
-		if (empty($___data_for_view)) {
-			$___data_for_view = $this->viewVars;
-		}
-		foreach($___data_for_view as $data => $value)
-		{
-			if(!is_object($data))
-			{
-				$this->Smarty->assign($data, $value);
-			}
-		}
+        if (empty($___data_for_view)) {
+            $___data_for_view = $this->viewVars;
+        }
+        foreach ($___data_for_view as $data => $value) {
+            if (!is_object($data)) {
+                $this->Smarty->assign($data, $value);
+            }
+        }
 
         switch ($this->smartyMajorVersion) {
             case 2:
@@ -140,30 +132,30 @@ class SmartyView extends View
                 break;
         }
 
-		$res = $this->Smarty->fetch($___viewFn);
-        
+        $res = $this->Smarty->fetch($___viewFn);
+
         return $res;
-	}
-	
-	/**
-	 * checks for existence of special method on loaded helpers, invoking it if it exists
-	 * this allows helpers to register smarty functions, modifiers, blocks, etc.
-	 */
-	function registerFunctions() {
+    }
+
+    /**
+     * checks for existence of special method on loaded helpers, invoking it if it exists
+     * this allows helpers to register smarty functions, modifiers, blocks, etc.
+     */
+    function registerFunctions() {
         /* check vector or hash tabe. */
         if (array_values($this->helpers) === $this->helpers) {
             $helpers = $this->helpers;
         } else {
             $helpers = array_keys($this->helpers);
         }
-		foreach($helpers as $helper) {
+        foreach ($helpers as $helper) {
             list($plugin, $helper) = $this->pluginSplit($helper);
-            $helperClass = $helper. 'Helper';
+            $helperClass = $helper . 'Helper';
             if (property_exists($this, $helper) && method_exists($this->{$helper}, '_registerSmartyFunctions')) {
                 $this->{$helper}->_registerSmartyFunctions($this->Smarty);
-			}
-		}
-	}
+            }
+        }
+    }
 
     /**
      * Set version of smarty
@@ -172,7 +164,7 @@ class SmartyView extends View
         // Version 2.6.x
         if (isset($this->Smarty->_version)) {
             $this->smartyVersion = $this->Smarty->_version;
-        } 
+        }
         // Version 3.1.x
         if (defined('Smarty::SMARTY_VERSION')) {
             $v = constant('Smarty::SMARTY_VERSION');
@@ -184,5 +176,7 @@ class SmartyView extends View
         }
         return $this->smartyVersion;
     }
+
 }
+
 ?>
